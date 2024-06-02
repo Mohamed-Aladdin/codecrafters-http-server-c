8265a5356e8c5df3c0aa7c468b1fa92ad44df4d2
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-06-02 09:36:38 +0000

    pass 6th stage

--- app/server.c
+++ app/server.c
@@ -6,8 +6,48 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 1024
+
+void *request_handler(void *cfd) {
+	int client_fd = *(int*)cfd;
+	char req_buffer[BUFFER_SIZE];
+	char res[BUFFER_SIZE];
+
+	if (read(client_fd, req_buffer, BUFFER_SIZE) < 0) {
+		printf("Read failed: %s \n", strerror(errno));
+		return NULL;
+	} else {
+		printf("Request from client: %s\n", req_buffer);
+	}
+
+	char *path = strtok(req_buffer, " ");
+	path = strtok(NULL, " ");
+	char *res_ok = "HTTP/1.1 200 OK\r\n\r\n";
+	char *res_not_found = "HTTP/1.1 404 Not Found\r\n\r\n";
+	char *format =
+		"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "
+		"%ld\r\n\r\n%s";
+	if (strncmp(path, "/user-agent", 11) == 0) {
+		strtok(0, "\r\n");
+		strtok(0, "\r\n");
+		char *userAgent = strtok(0, "\r\n") + 12;
+		sprintf(res, format, strlen(userAgent), userAgent);
+	} else if (strncmp(path, "/echo/", 6) == 0) {
+		char *content = path + 6;
+		sprintf(res, format, strlen(content), content);
+	} else {
+		snprintf(res, sizeof(res), "%s",
+			(strcmp(path, "/") == 0) ? res_ok : res_not_found);
+	}
+
+	int bytes_sent = send(client_fd, res, strlen(res), 0);
+
+	if (bytes_sent < 0) {
+		printf("Error: %s \n", strerror(errno));
+	}
+}
 
 int main() {
 	// Disable output buffering
@@ -17,10 +57,9 @@ int main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
-	// Uncomment this block to pass the first stage
-
 	int server_fd, client_addr_len;
 	struct sockaddr_in client_addr;
+	pthread_t tid;
 
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
@@ -37,9 +76,9 @@ int main() {
 	}
 
 	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									.sin_port = htons(4221),
-									.sin_addr = { htonl(INADDR_ANY) },
-									};
+					.sin_port = htons(4221),
+					.sin_addr = { htonl(INADDR_ANY) },
+	};
 
 	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
 		printf("Bind failed: %s \n", strerror(errno));
@@ -53,54 +92,28 @@ int main() {
 	}
 
 	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
 
-	const int client_fd =
-		accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	if (client_fd < 0) {
-		printf("Accept failed: %s \n", strerror(errno));
-		return 1;
-	}
-	printf("Client connected\n");
+	while (1) {
+			client_addr_len = sizeof(client_addr);
+			int *client_fd = malloc(sizeof(int));
+			if (!client_fd) {
+				printf("Memory allocation failed\n");
+				close(server_fd);
+				return 1;
+			}
+			*client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 
-	char req_buffer[BUFFER_SIZE];
-	char res[BUFFER_SIZE];
-
-	if (read(client_fd, req_buffer, BUFFER_SIZE) < 0) {
-		printf("Read failed: %s \n", strerror(errno));
-		return 1;
-	} else {
-		printf("Request from client: %s\n", req_buffer);
-	}
-
-	char *path = strtok(req_buffer, " ");
-	path = strtok(NULL, " ");
-
-	char *res_ok = "HTTP/1.1 200 OK\r\n\r\n";
-	char *res_not_found = "HTTP/1.1 404 Not Found\r\n\r\n";
-	char *format = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "
-                        "%ld\r\n\r\n%s";
-
-	if (strncmp(path, "/user-agent", 11) == 0) {
-		strtok(0, "\r\n");
-		strtok(0, "\r\n");
-		char *userAgent = strtok(0, "\r\n") + 12;
-		sprintf(res, format, strlen(userAgent), userAgent);
-	} else if (strncmp(path, "/echo/", 6) == 0) {
-		char *content = path + 6;
-		sprintf(res, format, strlen(content), content);
-	} else {
-		snprintf(res, sizeof(res), "%s", (strcmp(path, "/") == 0) ? res_ok : res_not_found);
+			if (client_fd < 0) {
+				printf("Accept failed: %s \n", strerror(errno));
+				free(client_fd);
+				close(server_fd);
+				return 1;
+			}
+			pthread_create(&tid, NULL, request_handler, client_fd);
+			pthread_detach(tid);
+			printf("Client connected\n");
 	}
 
-	int bytes_sent = send(client_fd, res, strlen(res), 0);
-
-	if (bytes_sent < 0) {
-		printf("Error: %s \n", strerror(errno));
-	}
-
-	close(server_fd);
-	close(client_fd);
-
-	return 0;
+	 close(server_fd);
+	 return 0;
 }
