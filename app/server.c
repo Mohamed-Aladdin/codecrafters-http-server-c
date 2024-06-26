6a1ae693d5f578523d7db2ab2782cdf902a19eec
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-06-26 18:02:45 +0000

    pass 8th stage

--- app/server.c
+++ app/server.c
@@ -41,7 +41,7 @@ void *request_handler(void *cfd) {
 		snprintf(f_path, sizeof(f_path), "%s/%s", dir, file);
 
 		// Read headers to get Content-Length
-		char *headers = strtok(NULL, "application/octet-stream\r\n\r\n");
+		char *headers = strtok(NULL, "application/octet-stream");
 		printf("Headers: %s, ReqBuff: %s", headers, req_buffer);
 		if (!headers) {
 			snprintf(res, sizeof(res), "%s", res_bad_request);
